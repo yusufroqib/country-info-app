d7895d6e92692abf9073870a250be30ded6bb894
yusufroqib <yusufroqib@gmail.com>
2023-09-29 11:32:38 +0100

    Functionality Updated

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const searchBtn = document.getElementById('search-btn')
-const result = document.getElementById('result')
-let input = document.getElementById('inputWord')
+const searchBtn = document.getElementById("search-btn");
+const result = document.getElementById("result");
+let input = document.getElementById("inputWord");
 
 // console.log(result.innerHTML);
 
-searchBtn.addEventListener('click', () => {
-    let inputWord = document.getElementById('inputWord').value
-    // console.log(inputWord);
-    fetch(`https://restcountries.com/v3.1/name/${inputWord}?fullText=true`)
-    .then((response)=> response.json())
+searchBtn.addEventListener("click", () => {
+  let inputWord = document.getElementById("inputWord").value;
+  // console.log(inputWord);
+  fetch(`https://restcountries.com/v3.1/name/${inputWord}?fullText=true`)
+    .then((response) => response.json())
     .then((data) => {
-        // console.log(data)
-        // console.log(data[0].currencies);
+      // console.log(data)
+      // console.log(data[0].currencies);
 
-        let currencies = data[0].currencies
-        let currency = Object.keys(currencies);
-        // console.log(currency);
+      let currencies = data[0].currencies;
+      let currency = Object.keys(currencies);
+      // console.log(currency);
 
-        // console.log(Array.from(currencies));
+      // console.log(Array.from(currencies));
 
-        let languages = data[0].languages
-        let language = Object.values(languages)
-        // console.log(language);
+      let languages = data[0].languages;
+      let language = Object.values(languages);
+      // console.log(language);
 
-        result.innerHTML = `
+      result.innerHTML = `
         <div class="imageContainer">
         <img src="${data[0].flags.png}" alt="country flag">
         <h2>${inputWord}</h2>
@@ -35,17 +35,17 @@ searchBtn.addEventListener('click', () => {
         <p class="eachInfo"><strong>Currencies:</strong> ${currency[0]}</p>
         <p class="eachInfo"><strong>Language:</strong> ${language[0]}</p>
     </div>
-        `
+        `;
     })
     .catch(() => {
-        result.innerHTML = `
+      result.innerHTML = `
             <h2 class="error">Couldn't Find The Country</h2>
-        `
-    })
-})
+        `;
+    });
+});
 
 input.addEventListener("keyup", function (event) {
-    if (event.keyCode == 13) {
-        searchBtn.click();
-    }
+  if (event.keyCode == 13) {
+    searchBtn.click();
+  }
 });
